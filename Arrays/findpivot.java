d4399955cf333844e6ded7556ae2eda034c674b0
Dharunkumar <dharunkumar.n2024cse@sece.ac.in>
2026-08-04 22:20:29 +0530

    learning arrays level 1

---
+++ Arrays/findpivot.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int total =0;
+        for(int num :nums){
+            total+=num;
+
+        }
+        int leftsum =0;
+        for(int i =0;i<nums.length;i++)
+        {
+             int right =  total - nums[i]- leftsum;
+             if(leftsum == right)
+             {
+                return i;
+             }
+
+             leftsum+=nums[i];
+        }
+
+        return -1;
+    }
+}
